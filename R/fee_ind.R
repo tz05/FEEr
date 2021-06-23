831402f1405add6a2858f582b3d846f0f9efb174
tz05 <tz05@me.com>
2021-06-23 00:36:04 -0500

    revise, debug communityMST, name change, vignette update

--- R/fee_ind.R
+++ R/fee_ind.R
@@ -4,7 +4,27 @@ globalVariables(c('coor_grids','i'))
 #'
 #' Computes the FEE index for the communities specified in \code{abund}, based
 #' on the trait data specified in \code{pool_traits}.
-#'
+#' 
+#' This function is the core function of the package. The \code{pool_traits}
+#' and \code{abund} are key arguments. The \code{dis_metric},
+#' \code{abundWeighted}, and \code{poolBased} are essential arguments, which
+#' together define the FEE calculation scheme. The other three arguments are
+#' auxiliary arguments, which might affect calculation efficiency or alter
+#' output format, but do not affect FEE calculation results.
+#' 
+#' Preparing eCDFs usually takes the most time of FEE calculation. To increase
+#' calculation speed, some pre-calculated eCDFs are enclosed in the package.
+#' For the cases with 1 to 6 traits, 2 to 90 species, and under the assumption
+#' of no prior knowledge about trait distribution (i.e., \code{poolBased} is
+#' \code{FALSE}, thus species pool does not affect eCDFs), these pre-calculated
+#' eCDFs are used in the FEE calculation. For other scenarios, especially
+#' species-pool-based calculation (\code{poolBased = TRUE}), eCDFs are not
+#' prepared and need to be calculated from scratch. In this case, specifying
+#' \code{user_ecdf} allows the newly calculated eCDFs to be saved and reused
+#' as needed. As a result, the saved eCDFs can considerably reduce calculation
+#' time for communities that are under the same settings of \code{pool_traits},
+#' \code{dis_metric}, and \code{poolBased}.
+#' 
 #' @param pool_traits a matrix (or data frame) of numeric functional traits for
 #'   species pool. Columns are for different traits and rows are for different
 #'   species in the species pool.
@@ -14,14 +34,14 @@ globalVariables(c('coor_grids','i'))
 #'   needs to be equal to the number of rows in \code{pool_traits}). Each row
 #'   is for a community. For species not presented in a community, their
 #'   abundance values should be assigned 0 or \code{NA}.
-#' @param abundWeighted logical; indicating whether the abundance values in
-#'   \code{abund} are considered as weights in FEE calculation. The default is
-#'   \code{TRUE}.
 #' @param dis_metric string specifying the scheme of quantifying species
 #'   dissimilarity in trait space. The currently available options are
 #'   \code{"euclidean"} (the default) and \code{"manhattan"}. Euclidean
 #'   distances are root sum-of-squares of differences, and manhattan distances
 #'   are the sum of absolute differences.
+#' @param abundWeighted logical; indicating whether the abundance values in
+#'   \code{abund} are considered as weights in FEE calculation. The default is
+#'   \code{TRUE}.
 #' @param poolBased logical; indicating whether the calculation is based on
 #'   species pool. The default is \code{FALSE}, indicating no prior knowledge
 #'   about trait distribution (i.e., adopting uniform distribution for trait
@@ -31,14 +51,14 @@ globalVariables(c('coor_grids','i'))
 #'   functional extension and evenness metric). The default is \code{FALSE}.
 #' @param user_ecdf string specifying the path and name of the file that saves
 #'   user's results of empirical cumulative distribution function (eCDF) of
-#'   FEE0. Under the same setting (\code{pool_traits} and \code{dis_metric}),
-#'   calculated eCDFs could be reused to save time. If the parameter is
+#'   FEE0. Under the same setting (\code{dis_metric} and \code{pool_traits}),
+#'   calculated eCDFs could be reused to save time. If the argument is
 #'   \code{NULL} (default), no user's eCDF will be adopted or saved, and the
-#'   species-pool-based eCDFs will be calculated from scratch. If the file is
-#'   given, the eCDFs contained in this file will be adopted as needed, and
-#'   when the eCDF of a specific combination of species number and trait number
-#'   is not available in the file, it will be calculated from scratch and then
-#'   saved to the file.
+#'   species-pool-based eCDFs will be calculated from scratch. If the file path
+#'   is provided, the eCDFs contained in this file will be loaded and used as
+#'   needed, and when the eCDF of a specific combination of species number and
+#'   trait number is not available in the file, it will be calculated from
+#'   scratch and then saved to the file.
 #' @param doParallel logical; indicating whether the calculation of FEE is
 #'   conducted in parallel. The default value is \code{FALSE}, i.e.,
 #'   sequentially calculating the FEE index for the communities specified in
@@ -46,10 +66,17 @@ globalVariables(c('coor_grids','i'))
 #' @return The FEE index values of the communities. If \code{doFEE0} is
 #'   \code{TRUE}, the output includes two lists: the raw FEE0 metrics and the
 #'   FEE indices. Specifically, FEE values for monocultures are \code{NA}.
+#' @examples
+#' d_trait <- cbind(tr1 = rnorm(10, 0, 1), tr2 = rnorm(10, 3, 2))
+#' d_comm <- rbind(comm1 = c(0:9),
+#'                 comm2 = rep(1, 10),
+#'                 comm3 = runif(10, 0, 5),
+#'                 comm4 = rchisq(10, 2))
+#' computeFEE(d_trait, d_comm, abundWeighted = TRUE)
 #' @import doSNOW
 #' @import foreach
 #' @export
-computeFEE <- function(pool_traits, abund, abundWeighted = TRUE, dis_metric = c("euclidean","manhattan"),
+computeFEE <- function(pool_traits, abund, dis_metric = c("euclidean","manhattan"), abundWeighted = TRUE,
                        poolBased = FALSE, doFEE0 = FALSE, user_ecdf = NULL, doParallel = FALSE) {
   dis_metric <- match.arg(dis_metric)
   # Assertion statements on input
@@ -57,16 +84,14 @@ computeFEE <- function(pool_traits, abund, abundWeighted = TRUE, dis_metric = c(
     stopifnot(nrow(pool_traits) == length(abund))
     abund <- matrix(abund,nrow=1)
   } else stopifnot(nrow(pool_traits) == ncol(abund))
-  ### need to warn when a community has only 1 species
   stopifnot(dis_metric %in% c("euclidean","manhattan"))
-
+  
   num_traits <- ncol(pool_traits)
-  nsp_comm <- apply(abund,1,function(x) length(which(x>0)))
-  uni_nsp_comm <- unique(nsp_comm)
+  nsp_comm <- computeNSP(abund)
   n_core_slurm <- as.integer(Sys.getenv("SLURM_CPUS_ON_NODE"))
   numCores <- parallel::detectCores()
   progress <- function(n) utils::setTxtProgressBar(pb,n)
-
+  
   std_pool_traits <- apply(pool_traits,2,function(x) (x-min(x))/(max(x)-min(x)))  # standardize traits to 0-1
   message(sprintf("\nCalculating MSTs for the %d communities...",length(nsp_comm)))
   pb <- utils::txtProgressBar(max = nrow(abund), initial = NA, style = 3)
@@ -87,7 +112,7 @@ computeFEE <- function(pool_traits, abund, abundWeighted = TRUE, dis_metric = c(
     }
   }
   close(pb)
-
+  
   message(sprintf("\nCalculating FEE0 for the %d communities...",nrow(abund)))
   pb <- utils::txtProgressBar(max = nrow(abund), initial = NA, style = 3)
   FEE0_comm <- function(mst_comm) {
@@ -116,11 +141,14 @@ computeFEE <- function(pool_traits, abund, abundWeighted = TRUE, dis_metric = c(
     } else message(sprintf("\nFile does not exist: %s",user_ecdf))
   }
   new_ecdf <- FALSE
-  nsp <- unique(nsp_comm)
+  uni_nsp_comm <- unique(nsp_comm)
+  if(1 %in% uni_nsp_comm) warning("\n  For monocultures, returning NA.")
+  ### need to warn when a community has only 1 species
+  
   if(poolBased) {
     message("\nPreparing pool-based eCDF...")
     if(!exists('usr_d',inherits=FALSE)) usr_d <- NULL
-    fee0_ecdf <- foreach(n=nsp) %do% {    # cannot be %dopar": need to be serialized
+    fee0_ecdf <- foreach(n=uni_nsp_comm) %do% {   # cannot be %dopar": need to be serialized
       nm_ecdf <- sprintf('%s_%dd_%dsp',substr(dis_metric,1,3),num_traits,n)
       if(nm_ecdf%in%names(usr_d)) usr_d[[nm_ecdf]]        # use usr_d data if available
       else {
@@ -138,7 +166,7 @@ computeFEE <- function(pool_traits, abund, abundWeighted = TRUE, dis_metric = c(
     if(num_traits<=6) sys_d <- getFromNamespace(sprintf('fee_ecdf_%s_%dd',substr(dis_metric,1,3),num_traits),'FEE') # pre-calcuated eCDFs in sysdata.rda
     else sys_d <- NULL
     if(!exists('uni_d',inherits=FALSE)) uni_d <- NULL
-    fee0_ecdf <- foreach(n=nsp) %do% {    # cannot be %dopar": need to be serialized
+    fee0_ecdf <- foreach(n=uni_nsp_comm) %do% {   # cannot be %dopar": need to be serialized
       nm_ecdf <- sprintf('%s_%dd_%dsp',substr(dis_metric,1,3),num_traits,n)
       if(nm_ecdf%in%names(sys_d)) sys_d[[nm_ecdf]]        # use sys_d data if available
       else if(nm_ecdf%in%names(uni_d)) uni_d[[nm_ecdf]]   # use uni_d data if available
@@ -159,7 +187,7 @@ computeFEE <- function(pool_traits, abund, abundWeighted = TRUE, dis_metric = c(
     else if(exists('usr_d',inherits=FALSE)) try(save(list='usr_d',file=user_ecdf))
     else if(exists('uni_d',inherits=FALSE)) try(save(list='uni_d',file=user_ecdf))
   }
-  names(fee0_ecdf) <- paste0('nsp_',nsp)
+  names(fee0_ecdf) <- paste0('nsp_',uni_nsp_comm)
   message(sprintf("\nCalculating FEE for the %d communities...",nrow(abund)))
   pb <- utils::txtProgressBar(max = nrow(abund), initial = NA, style = 3)
   FEE <- foreach(i=1:nrow(abund)) %do% {
@@ -184,6 +212,12 @@ computeFEE <- function(pool_traits, abund, abundWeighted = TRUE, dis_metric = c(
 #'   row corresponds a community. For species not presented in a community,
 #'   their abundance values should be assigned 0 or \code{NA}.
 #' @return Number of species in each of the communities specified in \code{abund}.
+#' @examples
+#' d_comm <- rbind(comm1 = c(0:9),
+#'                 comm2 = rep(1, 10),
+#'                 comm3 = runif(10, 0, 5),
+#'                 comm4 = rchisq(10, 2))
+#' computeNSP(d_comm)
 #' @export
 computeNSP <- function(abund) {
   return(apply(abund,1,function(x) length(which(x>0))))
@@ -221,9 +255,18 @@ coor_grids <- function(n,dim) {
 #' @return If \code{abundWeighted} is \code{TRUE}, return sorted branch
 #'   lengths of abundance-weighted MST. If \code{abundWeighted} is \code{FALSE},
 #'   return sorted branch lengths of raw MST.
+#' @examples
+#' d_trait <- cbind(tr1 = rnorm(10, 0, 1), tr2 = rnorm(10, 3, 2))
+#' comm1 = c(0:9)
+#' comm2 = rep(1, 10)
+#' communityMST(comm1, d_trait, abundWeighted = TRUE)
+#' communityMST(comm1, d_trait)
+#' communityMST(comm2, d_trait, abundWeighted = TRUE)
+#' communityMST(comm2, d_trait)
 #' @export
 communityMST <- function(abund, spp_traits, abundWeighted = TRUE, dis_metric = c("euclidean","manhattan")) {
   dis_metric <- match.arg(dis_metric)
+  abund <- as.vector(abund,mode="numeric")
   if(length(abund)==0|sum(abund)==0) return(list(NA,NA))
   stopifnot(length(abund) == nrow(spp_traits))
   stopifnot(sum(abund) > 0)
@@ -271,8 +314,15 @@ communityMST <- function(abund, spp_traits, abundWeighted = TRUE, dis_metric = c
 #'   species in the species pool. If \code{pool_traits} is \code{NULL}, the
 #'   calculation is then under the assumption of no prior knowledge of trait
 #'   values (i.e., uniform distribution of trait values).
-#' @details ...
 #' @return The eCDF of FEE0, used to transfer FEE0 metric to FEE index
+#' @examples
+#' d_trait <- cbind(tr1 = rnorm(10, 0, 1), tr2 = rnorm(10, 3, 2))
+#' ecdf1 <- FEE0_eCDF(3, 4)
+#' ecdf1(1.5)
+#' ecdf2 <- FEE0_eCDF(9, 2, pool_traits = d_trait)
+#' ecdf3 <- FEE0_eCDF(9, 2)
+#' ecdf2(1.5)
+#' ecdf3(1.5)
 #' @export
 FEE0_eCDF <- function(num_species, num_traits, dis_metric=c("euclidean","manhattan"), pool_traits=NULL) {
   if(num_species<=1) return(NA)
